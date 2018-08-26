2fe8995f61f6a69a50550a88da4459015947c925
Daniel4125 <Daniel4125@users.noreply.github.com>
2018-08-26 20:11:36 +0100

    Map and Sprites
    Map generation and tile placement

--- constants.py
+++ constants.py
@@ -1,11 +1,23 @@
 import pygame
 
+#GAME SIZES
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
 
-BLACK = ( 0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+#MAP
+MAP_WIDTH = 30
+MAP_HEIGHT = 30
+CELL_WIDTH = 32
+CELL_HEIGHT = 32
+
+COLOUR_BLACK = ( 0, 0, 0)
+COLOUR_WHITE = (255, 255, 255)
+COLOUR_RED = (255, 0, 0)
+COLOUR_GREEN = (0, 255, 0)
+COLOUR_BLUE = (0, 0, 255)
+
+#SPRITES
+S_PLAYER = pygame.image.load('sprites/player.png')
+S_WALL = pygame.image.load('sprites/wall1.png')
+S_FLOOR = pygame.image.load('sprites/floor.png')
 
--- main.py
+++ main.py
@@ -4,16 +4,70 @@ from constants import *
 
 clock = pygame.time.Clock()
 
-def initialize():
-	'''This function initializes the main window, and pygame'''
+
+
+class struct_Tile:
+	def __init__(self, block_path):
+		 self.block_path = block_path
+
+
+
+# __  __             
+#|  \/  | __ _ _ __  
+#| |\/| |/ _` | '_ \ 
+#| |  | | (_| | |_) |
+#|_|  |_|\__,_| .__/ 
+#             |_|    
+
+def create_map():
+	new_map = [[struct_Tile(False) for y in range(0, MAP_HEIGHT)] for x in range(0, MAP_WIDTH)]
+
+	new_map[10][10].block_path = True
+	new_map[10][15].block_path = True
+
+	return new_map
+
+
+# ____                     _             
+#|  _ \ _ __ __ ___      _(_)_ __   __ _ 
+#| | | | '__/ _` \ \ /\ / / | '_ \ / _` |
+#| |_| | | | (_| |\ V  V /| | | | | (_| |
+#|____/|_|  \__,_| \_/\_/ |_|_| |_|\__, |
+#                                  |___/
+
+def draw_game():
 	global DISPLAYSURF
-	pygame.init()
-	pygame.display.set_caption('pygame')
 
-	#set screen size
-	DISPLAYSURF = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+	#clear the surface
+	DISPLAYSURF.fill(COLOUR_BLUE)
 
-def update():
+	#draw the map
+	draw_map(GAME_MAP)
+
+	#draw the character
+	DISPLAYSURF.blit(S_PLAYER, (200,200))
+
+	#update the display
+	pygame.display.flip()
+
+def draw_map(map_to_draw):
+	for x in range(0,MAP_WIDTH):
+		for y in range(0,MAP_HEIGHT):
+			if map_to_draw[x][y].block_path == True:
+				#draw wall
+				DISPLAYSURF.blit((S_WALL), (x*CELL_WIDTH, y*CELL_HEIGHT))
+			else:
+				#draw floor
+				DISPLAYSURF.blit((S_FLOOR), (x*CELL_WIDTH, y*CELL_HEIGHT))
+
+
+#  ____                      
+# / ___| __ _ _ __ ___   ___ 
+#| |  _ / _` | '_ ` _ \ / _ \
+#| |_| | (_| | | | | | |  __/
+# \____|\__,_|_| |_| |_|\___|
+
+def main_loop():
 	'''The main game loop'''
 	while True:
 		# get player input
@@ -26,24 +80,29 @@ def update():
 
 		draw_game()
 		clock.tick(60)
+		
+def initialize_game():
+	'''This function initializes the main window, and pygame'''
+	global DISPLAYSURF, GAME_MAP
+	pygame.init()
+	pygame.display.set_caption('pygame')
+
+	#set screen size
+	DISPLAYSURF = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+	GAME_MAP = create_map()
 
 
-def draw_game():
-	global DISPLAYSURF
 
-	#clear the surface
-	DISPLAYSURF.fill(BLUE)
-
-	#TODO draw the map
-
-	#draw the character
-
-	#update the display
-	pygame.display.flip()
+# ____                  _             __  __       _       _ _ _ 
+#|  _ \ _   _ _ __   __| | ___ _ __  |  \/  | __ _(_)_ __ | | | |
+#| | | | | | | '_ \ / _` |/ _ \ '__| | |\/| |/ _` | | '_ \| | | |
+#| |_| | |_| | | | | (_| |  __/ |    | |  | | (_| | | | | |_|_|_|
+#|____/ \__,_|_| |_|\__,_|\___|_|    |_|  |_|\__,_|_|_| |_(_|_|_)
 
 
 if __name__ == '__main__':
-	initialize()
-	update()
+	initialize_game()
+	main_loop()
 
 
